83f5153a6fc3f4717d0f5f0102671df225c85d47
Ibrahim-ahmed1678 <ibrahimibbi77@gmail.com>
2025-08-11 15:56:11 -0700

    Fixed GUI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+#from pygame.locals import *
 #import pymunk
 # Import pymunk for physics
 
@@ -13,4 +14,9 @@ pygame.draw.circle(window, (255, 0, 0), (300, 300), 170, 0)
 #update the display
 pygame.display.update()
 
-# Quit Pygame
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+pygame.quit()
